88818a888cfc1f31b2249650423dfac73e05f144
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-09-29 12:49:32 +0200

    comment

--- NemID_ESB/esb_mock.js
+++ NemID_ESB/esb_mock.js
@@ -61,7 +61,8 @@ app.post('/generate-auth-code', async (req, res) =>{
 app.patch('/change-nemId-password', (req, res) => {
     let data = req.body;
     let query = "UPDATE user SET Password = ? WHERE CPR = ?";
-    db.run(query, [data.newPassword, data.cpr], (err) =>{
+    db.run(query, [data.newPassword, data.cpr], (err) =>{       // change: data.newPassword to data.nemIdPassword
+        // data.cpr ???
         if(err){
             console.log(err);
         }
