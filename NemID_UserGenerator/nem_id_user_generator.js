5ed60f0471f9b470405ef185df3f7dbea1bca150
Paul Panaitescu <paul.panaitescu.dk@gmail.com>
2020-09-29 15:24:58 +0200

    Finished Topic 4: NemID User Generator

---
+++ NemID_UserGenerator/nem_id_user_generator.js
@@ -0,0 +1,34 @@
+const express = require('express');
+// const sqlite3 = require('sqlite3');
+const PORT = 8088;
+let app = express();
+
+app.use(express.json());
+
+// Topic 4: NemID User Generator
+app.post("/generate-nemID", (req, res) => {
+    console.log("Body: ", req.body);
+    let cpr = req.body.cpr;
+    let email = req.body.email;
+    let randomFiveDigitNumber = Math.floor(10000 + Math.random() * 90000);
+    let nemId = randomFiveDigitNumber + cpr.slice(-4);
+    console.log("cpr", cpr);
+    console.log("email", email);
+    console.log("randomFiveDigitNumber", randomFiveDigitNumber);
+    console.log("nemId", nemId);
+
+    res.json({
+        status: 201,
+        message: 'Created NemID!',
+        nemId: nemId
+    });
+});
+
+app.listen(PORT, (err) => {
+    if(err){
+        console.log(err);
+    }
+    else{
+        console.log(`Listening on port ${PORT}`);
+    }
+});
